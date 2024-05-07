3a9dae8e6bf6f2647a51cefac4e3ff4c396f3a1e
kuro <devandresvargas@gmail.com>
2024-05-07 03:08:35 -0500

    fix

--- shell/init.sh
+++ shell/init.sh
@@ -5,18 +5,4 @@ source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
 
 
-# Customs
-#
 
-# setopt noclobber
-# # setopt autopushd
-# # Use emacs keybindings even if our EDITOR is set to vi
-# bindkey -e
-#
-# # Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
-# HISTSIZE=1000
-# SAVEHIST=1000
-# HISTFILE=~/.zsh_history
-# setopt histignorealldups
-# setopt sharehistory
-# unset HISTFILE
