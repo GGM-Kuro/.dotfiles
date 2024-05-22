df042a2a23815313bd001cc2d52e334380bfb629
kuro <devandresvargas@gmail.com>
2024-05-22 00:30:53 -0500

    prepare flutter enviroment

---
+++ os/linux/wezterm/kuro/keymaps.lua
@@ -0,0 +1,29 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+-- 36 is the default, but you can choose a different size.
+-- Uses the same font as window_frame.font
+-- config.pane_select_font_size=36,
+
+local keys = {
+  -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
+  { key = '8', mods = 'CTRL', action = act.PaneSelect },
+  -- activate pane selection mode with numeric labels
+  {
+    key = '9',
+    mods = 'CTRL',
+    action = act.PaneSelect {
+      alphabet = '1234567890',
+    },
+  },
+  -- show the pane selection mode, but have it swap the active and selected panes
+  {
+    key = '0',
+    mods = 'CTRL',
+    action = act.PaneSelect {
+      mode = 'SwapWithActive',
+    },
+  },
+}
+
+return keys
--- os/linux/wezterm/wezterm.lua
+++ os/linux/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local settings = require 'kuro.settings'
+local keymaps= require 'kuro.keymaps'
 local config = {}
 
 if wezterm.config_builder then
@@ -12,5 +13,6 @@ for k, v in pairs(settings) do
   config[k] = v
 end
 
+config.keys = keymaps
 
 return config
