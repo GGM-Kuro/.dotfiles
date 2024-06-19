8481d69843ba24de6c42a3e12a4ff8b3ee776567
GGM-Kuro <devandres@vargas.com>
2024-06-19 03:32:04 -0500

    add aliases and fix zellij aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -11,8 +11,10 @@ alias ls='lsd --group-dirs=first'
 alias cat='bat'
 alias ~="cd ~"
 alias dotfiles='cd $DOTFILES_PATH'
+alias .f='cd $DOTFILES_PATH'
 
 # Git
+alias ga="git add"
 alias gaa="git add -A"
 alias gc='$DOTLY_PATH/bin/dot git commit'
 alias gca="git add --all && git commit --amend --no-edit"
@@ -32,7 +34,7 @@ alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
 alias up='dot package update_all'
-alias pacman-npm= 'sudo pacman --overwrite "*" -S npm'
+alias pacman-npm= 'pacman --overwrite "*" -S npm'
 alias pdf="mupdf"
 alias pj='cd $PROJECT_PATHS/$(dot kuro pj)'
 alias ff='yazi'
@@ -41,6 +43,17 @@ alias ff='yazi'
 alias rs='dot screens reload_wallpaper'
 alias msi='dot screens msi'
 alias main='dot screens main'
-alias zs='zellij attach $(zellij list-sessions -s | fzf)'
-alias zq='zellij kill-session $(zellij list-sessions -s | fzf)'
-alias zqa='zellij kill-all-sessions'
+alias za='zellij attach $(zellij list-sessions -s | fzf)'
+alias zs='zellij list-sessions '
+alias zk="zellij kill-session \$(zellij list-sessions | awk '!/EXITED/ {print \$1}' | sed -r 's/\x1B\[[0-9;]*[mGK]//g' | fzf)"
+alias zka='zellij kill-all-sessions'
+alias zd="zellij delete-session \$(zellij list-sessions | awk '/EXITED/ {print \$1}' | sed -r 's/\x1B\[[0-9;]*[mGK]//g' | fzf)"
+alias zda='zellij delete-all-sessions'
+
+# nvim
+alias v='nvim'
+alias vim='nvim'
+alias vi='nvim'
+alias vimrc='nvim ~/.config/nvim/init.vim'
+alias zshrc='nvim ~/.zshrc'
+alias vpicom= 'nvim ~/.config/picom/picom.conf'
